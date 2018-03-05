0806770945090bac47b1444045476978e197dd9d
lordey <lord.dey68@gmail.com>
2018-03-05 23:58:57 +0700

    first commit

---
+++ menu.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+#Menu
+cname=$( awk -F: '/model name/ {name=$2} END {print name}' /proc/cpuinfo )
+cores=$( awk -F: '/model name/ {core++} END {print core}' /proc/cpuinfo )
+freq=$( awk -F: ' /cpu MHz/ {freq=$2} END {print freq}' /proc/cpuinfo )
+tram=$( free -m | awk 'NR==2 {print $2}' )
+swap=$( free -m | awk 'NR==4 {print $2}' )
+up=$(uptime|awk '{ $1=$2=$(NF-6)=$(NF-5)=$(NF-4)=$(NF-3)=$(NF-2)=$(NF-1)=$NF=""; print }')
+echo -e "************************************"
+echo -e "\e[032;1mCPU model:\e[0m $cname"
+echo -e "\e[032;1mNumber of cores:\e[0m $cores"
+echo -e "\e[032;1mCPU frequency:\e[0m $freq MHz"
+echo -e "\e[032;1mTotal amount of ram:\e[0m $tram MB"
+echo -e "\e[032;1mTotal amount of swap:\e[0m $swap MB"
+echo -e "\e[032;1mSystem uptime:\e[0m $up"
+echo -e "\e[032;1mInstaler By:\e[0m \e[037;1mDey Agustian\e[0m"
+echo -e "\e[032;1mAll Supported By:\e[0m \e[037;1mWrgSGntng\e[0m"
+echo -e "************************************"
+echo -e "KONFIGURASI SERVER 	:" 
+echo -e "\e[031;1m+ open SSH [22, 444]"
+echo -e "\e[032;1m+ dropbear [109, 110, 22507, 53]"
+echo -e "\e[033;1m+ SSL/TLS [443]"  
+echo -e  "\e[034;1m+ squid3 [80, 8080]"  
+echo -e  "\e[035;1m+ badvpn-udpgw [7300]"  
+echo -e  "\e[036;1m+ webmin [http://$MYIP:10000/]"  
+echo -e  "\e[031;1m+ timezone [Asia/Jakarta (GMT \e[032;+7]"  
+echo -e  "\e[032;1m+ IPv6 [off]"  
+echo -e  "************************************" 
+echo -e  "COMMAND LIST 	:"  
+echo -e  "\e[033;1mx menampilkan command list/daftar perintah [menu]"  
+echo -e  "\e[034;1mx membuat akun baru [usernew]"  
+echo -e  "\e[035;1mx membuat akun trial berdurasi 1 hari [trial]"  
+echo -e  "\e[036;1mx mengahpus akun ssh [hapus]"  
+echo -e  "\e[031;1mx memeriksa user yang login [cek]"  
+echo -e  "\e[032;1mx memeriksa daftar user [member]"  
+echo -e  "\e[033;1mx restart service dropbear, webmin, squid3, ssh, & OpenVPN [resvis]"  
+echo -e  "\e[034;1mx reboot server [reboot]"  
+echo -e  "\e[035;1mx test speed server [speedtest]"  
+echo -e  "\e[036;1mx informasi script [about]"  
+echo -e  "************************************" 
+echo -e  "\e[031;1mBuilder : Dey Agustian"  
+echo -e  "\e[032;1mThx to  : Fornesia, Rzengineer, Fawzya"  
+echo -e  "\e[037;1m************************************" 
---
+++ original.sh
@@ -0,0 +1,203 @@
+#!/bin/bash
+#
+# Builder Dey Agustian
+# Thx to fornesia, rzengineer and fawzya
+# ==================================================
+
+# update dan upgrade
+apt-get -y update && apt-get -y upgrade
+
+# initialisasi var
+export DEBIAN_FRONTEND=noninteractive
+OS=`uname -m`;
+MYIP=$(wget -qO- ipv4.icanhazip.com);
+MYIP2="s/xxxxxxxxx/$MYIP/g";
+
+#detail nama perusahaan
+country=ID
+state=WestJava
+locality=Majalengka
+organization=WarungSiGanteng
+organizationalunit=IT
+commonname=DeyAgustian
+email=admin@warungsiganteng.com
+
+# go to root
+cd
+
+# disable ipv6
+echo 1 > /proc/sys/net/ipv6/conf/all/disable_ipv6
+sed -i '$ i\echo 1 > /proc/sys/net/ipv6/conf/all/disable_ipv6' /etc/rc.local
+
+# install wget and curl
+apt-get update;apt-get -y install wget curl;
+
+# set time GMT +7
+ln -fs /usr/share/zoneinfo/Asia/Jakarta /etc/localtime
+
+# set locale
+sed -i 's/AcceptEnv/#AcceptEnv/g' /etc/ssh/sshd_config
+service ssh restart
+
+# set repo
+wget -O /etc/apt/sources.list "https://raw.githubusercontent.com/lordey/wrgsgntng-conf/master/sources.list.debian7"
+wget "http://www.dotdeb.org/dotdeb.gpg"
+cat dotdeb.gpg | apt-key add -;rm dotdeb.gpg
+
+# update
+apt-get update
+
+# install neofetch
+echo "deb http://dl.bintray.com/dawidd6/neofetch jessie main" | sudo tee -a /etc/apt/sources.list
+curl -L "https://bintray.com/user/downloadSubjectPublicKey?username=bintray" -o Release-neofetch.key && sudo apt-key add Release-neofetch.key && rm Release-neofetch.key
+apt-get update
+apt-get install neofetch
+echo "clear" >> .bashrc
+echo 'echo -e "Selamat datang di server $HOSTNAME"' >> .bashrc
+echo 'echo -e "ketik menu untuk menampilkan perintah..."' >> .bashrc
+echo "menu" >> .bashrc
+echo 'echo -e ""' >> .bashrc
+
+# install badvpn
+cd
+wget -O /usr/bin/badvpn-udpgw "https://raw.githubusercontent.com/lordey/badvpn/master/badvpn-udpgw"
+if [ "$OS" == "x86_64" ]; then
+  wget -O /usr/bin/badvpn-udpgw "https://raw.githubusercontent.com/lordey/badvpn/master/badvpn-udpgw64"
+fi
+sed -i '$ i\screen -AmdS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7300' /etc/rc.local
+chmod +x /usr/bin/badvpn-udpgw
+screen -AmdS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7300
+
+# setting port ssh
+cd
+sed -i 's/Port 22/Port 22/g' /etc/ssh/sshd_config
+sed -i '/Port 22/a Port 444' /etc/ssh/sshd_config
+service ssh restart
+
+# install dropbear
+apt-get -y install dropbear
+sed -i 's/NO_START=1/NO_START=0/g' /etc/default/dropbear
+sed -i 's/DROPBEAR_PORT=22/DROPBEAR_PORT=109/g' /etc/default/dropbear
+sed -i 's/DROPBEAR_EXTRA_ARGS=/DROPBEAR_EXTRA_ARGS="-p 110 -p 22507 -p 53"/g' /etc/default/dropbear
+echo "/bin/false" >> /etc/shells
+echo "/usr/sbin/nologin" >> /etc/shells
+service ssh restart
+service dropbear restart
+
+# install squid3
+cd
+apt-get -y install squid3
+wget -O /etc/squid3/squid.conf "https://raw.githubusercontent.com/lordey/wrgsgntng-conf/master/squid3.conf"
+sed -i $MYIP2 /etc/squid3/squid.conf;
+service squid3 restart
+
+# install webmin
+cd
+apt-get -y install perl libnet-ssleay-perl openssl libauthen-pam-perl libpam-runtime libio-pty-perl apt-show-versions python
+wget https://raw.githubusercontent.com/lordey/memek/master/webmin_1.870_all.deb
+dpkg --install webmin_1.870_all.deb
+sed -i 's/ssl=1/ssl=0/g' /etc/webmin/miniserv.conf
+rm -f webmin_1.870_all.deb
+service webmin restart
+
+# install stunnel
+apt-get install stunnel4 -y
+cat > /etc/stunnel/stunnel.conf <<-END
+cert = /etc/stunnel/stunnel.pem
+client = no
+socket = a:SO_REUSEADDR=1
+socket = l:TCP_NODELAY=1
+socket = r:TCP_NODELAY=1
+
+
+[dropbear]
+accept = 443
+connect = 127.0.0.1:109
+
+END
+
+#membuat sertifikat
+openssl genrsa -out key.pem 2048
+openssl req -new -x509 -key key.pem -out cert.pem -days 1095 \
+-subj "/C=$country/ST=$state/L=$locality/O=$organization/OU=$organizationalunit/CN=$commonname/emailAddress=$email"
+cat key.pem cert.pem >> /etc/stunnel/stunnel.pem
+
+#konfigurasi stunnel
+sed -i 's/ENABLED=0/ENABLED=1/g' /etc/default/stunnel4
+/etc/init.d/stunnel4 restart
+
+# download script
+cd /usr/bin
+wget -O menu "https://raw.githubusercontent.com/lordey/wrgsgntng-ins/master/menu.sh"
+wget -O usernew "https://raw.githubusercontent.com/lordey/wrgsgntng-conf/master/usernew.sh"
+wget -O trial "https://raw.githubusercontent.com/lordey/wrgsgntng-conf/master/trial.sh"
+wget -O hapus "https://raw.githubusercontent.com/lordey/wrgsgntng-conf/master/hapus.sh"
+wget -O cek "https://raw.githubusercontent.com/lordey/wrgsgntng-conf/master/user-login.sh"
+wget -O member "https://raw.githubusercontent.com/lordey/wrgsgntng-conf/master/user-list.sh"
+wget -O resvis "https://raw.githubusercontent.com/lordey/wrgsgntng-conf/master/resvis.sh"
+wget -O speedtest "https://raw.githubusercontent.com/lordey/wrgsgntng-conf/master/speedtest_cli.py"
+wget -O info "https://raw.githubusercontent.com/lordey/wrgsgntng-conf/master/info.sh"
+wget -O about "https://raw.githubusercontent.com/lordey/wrgsgntng-conf/master/about.sh"
+
+echo "0 0 * * * root /sbin/reboot" > /etc/cron.d/reboot
+
+chmod +x menu
+chmod +x usernew
+chmod +x trial
+chmod +x hapus
+chmod +x cek
+chmod +x member
+chmod +x resvis
+chmod +x speedtest
+chmod +x info
+chmod +x about
+
+# swap
+dd if=/dev/zero of=/swapfile bs=1024 count=2048k
+mkswap /swapfile
+swapon /swapfile
+echo '/swapfile swap swap defaults 0 0' >> /etc/fstab
+chmod 0600 /swapfile
+free -m
+
+# finishing
+cd
+chown -R www-data:www-data /home/vps/public_html
+service cron restart
+service ssh restart
+service dropbear restart
+service squid3 restart
+service webmin restart
+rm -rf ~/.bash_history && history -c
+echo "unset HISTFILE" >> /etc/profile
+
+# info
+clear
+echo "************************************" | tee -a log-install.txt
+echo "KONFIGURASI SERVER 	:"  | tee -a log-install.txt
+echo "+ open SSH [22, 444]"  | tee -a log-install.txt
+echo "+ dropbear [109, 110, 22507, 53]"  | tee -a log-install.txt
+echo "+ SSL/TLS [443]"  | tee -a log-install.txt
+echo "+ squid3 [80, 8080]"  | tee -a log-install.txt
+echo "+ badvpn-udpgw [7300]"  | tee -a log-install.txt
+echo "+ webmin [http://$MYIP:10000/]"  | tee -a log-install.txt
+echo "+ timezone [Asia/Jakarta (GMT +7]"  | tee -a log-install.txt
+echo "+ IPv6 [off]"  | tee -a log-install.txt
+echo "************************************" | tee -a log-install.txt
+echo "COMMAND LIST 	:"  | tee -a log-install.txt
+echo "x menampilkan command list/daftar perintah [menu]"  | tee -a log-install.txt
+echo "x membuat akun baru [usernew]"  | tee -a log-install.txt
+echo "x membuat akun trial berdurasi 1 hari [trial]"  | tee -a log-install.txt
+echo "x mengahpus akun ssh [hapus]"  | tee -a log-install.txt
+echo "x memeriksa user yang login [cek]"  | tee -a log-install.txt
+echo "x memeriksa daftar user [member]"  | tee -a log-install.txt
+echo "x restart service dropbear, webmin, squid3, ssh, & OpenVPN [resvis]"  | tee -a log-install.txt
+echo "x reboot server [reboot]"  | tee -a log-install.txt
+echo "x test speed server [speedtest]"  | tee -a log-install.txt
+echo "x informasi script [about]"  | tee -a log-install.txt
+echo "************************************" | tee -a log-install.txt
+echo "Builder : Dey Agustian"  | tee -a log-install.txt
+echo "Thx to  : Fornesia, Rzengineer, Fawzya"  | tee -a log-install.txt
+echo "************************************" | tee -a log-install.txt
+cd
+rm -f /root/debian7.sh
